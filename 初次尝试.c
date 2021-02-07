ad346cb5249581f34c9199f59baf774078a0a802
Cbiltps <78654623+Cbiltps@users.noreply.github.com>
2021-02-07 13:42:18 +0800

    Add files via upload

---
+++ 初次尝试.c
@@ -0,0 +1,14 @@
+#include<stdio.h>//#是一种指令形式 include包含一个叫stdio.h的文件
+//int是整型的意思
+//main前面的int表示main函数调用返回一个整数值
+int main()//主函数-程序的入口-main函数有且仅有一个
+{
+	printf("hello world\n");//在屏幕上输出hello world
+	return 0;//返回 0
+}
+
+//void main() void 这个写法已经过时了
+//{
+//这里完成任务
+//return 0;
+//}
