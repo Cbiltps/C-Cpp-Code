e5f9009840e20941da7d59b315b7287070c23a99
李承祥 <78654623+Cbiltps@users.noreply.github.com>
2021-06-01 14:35:30 +0800

    Add files via upload

---
+++ 数组元素逆序输出.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i, a[10];
+	for (i = 0; i <= 9; i++)
+	{
+		a[i] = i;
+	}
+	for (i = 9; i >= 0; i--)
+	{
+		printf("%3d", a[i]);
+	}
+	printf("\n");
+	return 0;
+}
