d7f2edcd470bd46cdae555409a7f2ef991955523
李承祥 <78654623+Cbiltps@users.noreply.github.com>
2021-06-10 17:43:38 +0800

    Add files via upload

---
+++ 数据类型占内存大小.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("The size of short is %d bytes.\n", sizeof(short));//计算大小的操作符
+	printf("The size of int is %d bytes.\n", sizeof(int));
+	printf("The size of long is %d bytes.\n", sizeof(long));
+	printf("The size of long long is %d bytes.\n", sizeof(long long));
+	return 0;
+}
