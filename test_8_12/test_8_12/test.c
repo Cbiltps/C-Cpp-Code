bf3e554af697b86bac72fd4ae385157fcb44a70d
Cbiltps <1239669254@qq.com>
2021-08-13 20:21:38 +0800

    博客（内存）

---
+++ test_8_12/test_8_12/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//void test(void)
+//{
+//	printf("hehe\n");
+//}
+//
+//int main()
+//{
+//	test();
+//	test(100);
+//
+//	return 0;
+//}
+
+int main()
+{
+	int a = 0x11223344;
+
+	return 0;
+}
