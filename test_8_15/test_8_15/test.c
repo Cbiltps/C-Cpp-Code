b78aa7b4a828ecf1f7e277e2848e7ba95ce8a861
Cbiltps <1239669254@qq.com>
2021-08-15 23:33:16 +0800

    博客素材代码（if语句）

---
+++ test_8_15/test_8_15/test.c
@@ -0,0 +1,100 @@
+#define _CRT_SECURE_NO_DEPRECATE 1
+
+//int main()
+//{
+//	int age = 10;
+//
+//	if (age < 18)
+//		printf("未成年\n");
+//	if (age >= 18 && age < 30)
+//		printf("青年\n");
+//	if (age >= 30 && age < 55)
+//		printf("中年\n");
+//	if (age >= 55 && age < 70)
+//		printf("中老年\n");
+//	if (age >= 70 && age < 100)
+//		printf("老年\n");
+//	if(age>=100)
+//		printf("老寿星\n");
+//
+//	/*if (age < 18)
+//		printf("未成年\n");
+//	else if (age >= 18 && age < 30)
+//		printf("青年\n");
+//	else if (age >= 30 && age < 55)
+//		printf("中年\n");
+//	else if (age >= 55 && age < 70)
+//		printf("中老年\n");
+//	else if (age >= 70 && age < 100)
+//		printf("老年\n");
+//	else
+//		printf("老寿星\n");*/
+//
+//	//if (age >= 18)
+//	//	printf("成年\n");
+//	//else
+//	//	printf("未成年\n");
+//
+//
+//	/*if (age >= 18)
+//		printf("成年\n");*/
+//
+//	return 0;
+//}
+
+//if (age >= 18)
+//	{
+//		printf("成年\n");
+//		printf("可以谈恋爱\n");
+//	}
+//	else
+//	{
+//		printf("未成年\n");
+//	}
+
+//#include <stdio.h>
+//int main()
+//{
+//	if (表达式)
+//	{
+//		语句列表1；
+//	}
+//	else
+//	{
+//		语句列表2；
+//	}
+//	return 0;
+//}
+//
+#include <stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 2;
+	if (a == 1)
+		if (b == 2)
+			printf("hehe\n");
+		else
+			printf("haha\n");
+	return 0;
+}
+//上面的是错误的代码
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 2;
+//	if (a == 1)
+//	{
+//		if (b == 2)
+//		{
+//			printf("hehe\n");
+//		}
+//	}
+//	else
+//	{
+//		printf("haha\n");
+//	}
+//	return 0;
+//}
